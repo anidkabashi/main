6b15ff52ca3fbba8b971b99ce9885da819e4836a
anidkabashi <anidkabashi@gmail.com>
2025-01-27 03:18:48 -0800

    end

---
+++ lesson26/main.js
@@ -0,0 +1,67 @@
+console.log("Goodbye");
+
+//var x=5;
+
+var a= 50 + 50;
+console.log(a);
+
+var b=20;
+var c=30;
+var d= b + c;
+console.log(d);
+
+
+var e=40;
+var f=10;
+var g= e - f;
+console.log(g);
+
+var h= 100;
+var i=5;
+var j=h/i;
+console.log(j);
+
+var k=10;
+var l=4;
+var m= k % 1;
+console.log(m);
+
+var n=99;
+m++;
+console.log(n);
+
+var o=101;
+0--;
+console.log(o);
+
+var p=20;
+var q= p**2;
+console.log(q);
+
+var x=15;
+x +=5;
+console.log(x);
+
+var s=10;
+s *=7;
+console.log(s);
+
+var v=5;
+var w=5;
+console.log(v ==w);
+
+var z=6;
+var t=8;
+console.log(z != t);
+
+var num1 =100;
+var num2 =10;
+console.log(num1 > num2);
+
+var num3=1;
+var num4= 2;
+console.log(num4< num3);
+
+var num5=123;
+var num6="123";
+console.log(num5 === num6);
