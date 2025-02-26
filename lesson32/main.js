1cd0b0cbcc3aeee7e8966a4ade8ec914bd85d1c9
anidkabashi <anidkabashi@gmail.com>
2025-02-26 12:16:04 +0100

    .

---
+++ lesson32/main.js
@@ -0,0 +1,211 @@
+var input = document.getElementById("input_id");
+var button =document.getElementById("btn_id");
+var text =document.getElementById("teksti_id");
+
+button.onclick= function(){
+    text.innerHTML = input.value;
+}
+
+// var text= "The best school in the world os digital school";
+// var result= text.search("Digital school");
+// document.getElementById("result1").innerHTML=result;
+
+
+// var text= "The best school in the world os digital school";
+// var result= text.search(/Digital school/);
+// document.getElementById("result2").innerHTML=result;
+
+
+
+// var text= "The best school in the world os digital school";
+// var result= text.replace(/Digital school/,"another school");
+// document.getElementById("result3").innerHTML=result;
+
+
+// var text ="my school is the best school in the world";
+// var regex= /school/g;
+// document.getElementById("result4").innerHTML=text.match(regex);
+
+// var text="Digital school is the best school in the world";
+// var regex=/i/g;
+// document.getElementById("result5").innerHTML=text.mach(regex);
+
+
+// var text="Digital school is the best school in the world";
+// var regex=/[abc]/g;
+// document.getElementById("result7").innerHTML=text.mach(regex);
+
+// var text="Digital school is in top 10 schools of the world";
+// var regex=/[0-9]/g;
+// document.getElementById("result8").innerHTML=text.mach(regex);
+
+
+// var text="Digital school is in top 10 schools of the world";
+// var regex=/(top|best|school)/g;
+// document.getElementById("result9").innerHTML=text.mach(regex);
+
+
+// var text="100 percent";
+// var regex=/\d/g;
+// document,getElementById("result10").innerHTML=text.mach(regex);
+
+
+// var text="Digital school is in top 10 schools of the world";
+// var regex=/\s/g;
+// document.getElementById("resul11").innerHTML=text.mach(regex);
+
+// var text="heeey, how are you";
+// var gegex=/e+/g;
+// document.getElementById("resul12").innerHTML=text.mach(regex);
+
+// var text="so, i hope we'll se each other again soon... ";
+// var gegex=/so*/g;
+// document.getElementById("resul13").innerHTML=text.mach(regex);
+
+
+// var text="hey, hi , hii";
+// var gegex=/hi?/g;
+// document.getElementById("resul14").innerHTML=text.mach(regex);
+
+// var text="hello, helloo, hrllooo";
+// var gegex=/o{3}/g;
+// document.getElementById("resul15").innerHTML=text.mach(regex);
+
+// var text="hell0 , helloo, hrllooo, hellooooo";
+// var gegex=/{3,5}/g;
+// document.getElementById("resul16").innerHTML=text.mach(regex);
+
+
+// var text="bestfriend, boyfreiend, girlfriend";
+// var gegex=/end$/g;
+// document.getElementById("resul17").innerHTML=text.mach(regex);
+
+// var text="vat, catalog, category, girlfriend";
+// var gegex=/^cat/g;
+// document.getElementById("resul18").innerHTML=text.mach(regex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
